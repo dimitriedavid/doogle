02eb5081b5d0cf27593c04ce5f3019bb27007098
Dimitrie David <david.dimitrie@gmail.com>
2020-02-24 12:25:44 +0200

    Updated no result found page

--- web/response.js
+++ web/response.js
@@ -7,10 +7,13 @@ eel.get_response(search_input)(handleResponse)
 
 async function handleResponse(response) {
     let decoded_response = JSON.parse(response);
-
-    for (const res of decoded_response['results']) {
-        words = await eel.getWordsFromFile(30, res)();
-        document.getElementById("hereGoesEverything").innerHTML += '<div class="jumbotron"><h4><b>' + res + '</b></h1><p>' + words + '</p><a class="btn btn-primary btn-sm" href="/docs/' + res + '" role="button">Open document</a></div>';
+    if(decoded_response['results'].length == 0) {
+        document.getElementById("hereGoesEverything").innerHTML += '<h4>No results found</h4>';
+    } else {
+        for (const res of decoded_response['results']) {
+            words = await eel.getWordsFromFile(30, res)();
+            document.getElementById("hereGoesEverything").innerHTML += '<div class="jumbotron"><h4><b>' + res + '</b></h1><p>' + words + '</p><a class="btn btn-primary btn-sm" href="/docs/' + res + '" role="button">Open document</a></div>';
+        }
     }
     document.getElementById("hereGoesEverything").innerHTML += '<p>Search took: ' + decoded_response['response_time'] / 1000 + ' milliseconds</p>';
 }
